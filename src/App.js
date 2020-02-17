b311f564eae944fa1554447d41e2aaebe0a6d7b8
Josh Bryson <josh@bryson.me.uk>
2020-02-17 19:59:48 +0000

    Header component added and extrapolation of InstancedPoints

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import Header from './components/Header/Header';
 import ThreePointVis from './components/ThreePointVis/ThreePointVis';
 import './App.css';
 
-const data = new Array(1000).fill(0).map((d, id) => ({ id }))
+const data = new Array(100000).fill(0).map((d, id) => ({ id }));
 
 function App() {
   return (
+    <React.Fragment>
+    <Header />
     <div className="App">
       <div className="vis-container">
         <ThreePointVis data={data} />
       </div>
     </div>
+    </React.Fragment>
   );
 }
 
--- src/components/ThreePointVis/ThreePointVis.js
+++ src/components/ThreePointVis/ThreePointVis.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Canvas } from 'react-three-fiber';
 import Controls from './Controls';
+import InstancedPoints from './InstancedPoints';
 
 const ThreePointVis = ({ data }) => {
   return (
-    <Canvas camera={{ position: [0, 0, 5] }}>
+    <Canvas camera={{ position: [0, 0, 80], far: 15000 }}>
       <Controls />
       <ambientLight color="#ffffff" intensity={0.1} />
       <hemisphereLight
@@ -13,24 +14,7 @@ const ThreePointVis = ({ data }) => {
         groundColor="#080820"
         intensity={1.0}
       />
-      {data.map((d, i) => {
-        const x = (i % 30) * 1.05;
-        const y = Math.floor(i / 30) * 1.05;
-        const z = 0;
-        return (
-          <mesh
-            key={d.id}
-            position={[x, y, z]} 
-            rotation={[Math.PI * 0.5, 0, 0]}
-          >
-            <cylinderBufferGeometry 
-              attach="geometry" 
-              args={[0.5, 0.5, 0.15, 32]} 
-            />
-            <meshStandardMaterial attach="material" color="#fff" />
-          </mesh>
-        )
-      })}
+      <InstancedPoints data={data} />
     </Canvas>
   )
 }
